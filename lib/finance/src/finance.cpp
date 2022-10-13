914cba9e6156f9c51aee043cc80915370048c57b
Dirk Arnez <smalldirkalex@gmail.com>
2022-10-13 22:06:22 +0800

    - update lib/finance/src/finance.cpp

--- lib/finance/src/finance.cpp
+++ lib/finance/src/finance.cpp
@@ -1,11 +1,19 @@
 #include "finance.h"
 
 #include <iostream>
-#include <cmath>
+
+// Type your code here, or load an example.
+constexpr long double power(long double base, unsigned int exponent) {
+    long double internal = 1;
+    for (unsigned int i = 0; i < exponent; i++){
+        internal *= base;
+    }
+    return internal;
+}
 
 constexpr long double calculate_future_value_compounded(long double present_value, unsigned int annual_precentage_rate_precentage, unsigned int nth_year)
 {
-  return present_value * std::pow((1 + (annual_precentage_rate_precentage / 100.0)), nth_year);
+  return present_value * power((1 + (annual_precentage_rate_precentage / 100.0)), nth_year);
 }
 
 constexpr long double calculate_future_value_single(long double present_value, unsigned int annual_precentage_rate_precentage, unsigned int nth_year)
@@ -16,6 +24,8 @@ constexpr long double calculate_future_value_single(long double present_value, u
 int main()
 {
     constexpr long double captial = 10000;
-    std::cout << calculate_future_value_compounded(captial, 5, 3) << std::endl; //10000*1.05*1.05*1.05 = 11576.25
-    std::cout << calculate_future_value_single(captial, 5, 3) << std::endl; //10000*(1 + (0.05 * 3)) = 11500
+    const long double a = calculate_future_value_compounded(captial, 5, 3);  //10000*1.05*1.05*1.05 = 11576.25
+    const long double b = calculate_future_value_single(captial, 5, 3); //10000*(1 + (0.05 * 3)) = 11500
+    std::cout << a << std::endl;
+    std::cout << b << std::endl; 
 }
